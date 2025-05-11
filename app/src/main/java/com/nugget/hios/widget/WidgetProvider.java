2e9bbb1852ce6ef0a40e71cd1445358d5fb99374
aarjay123 <76173037+aarjay123@users.noreply.github.com>
2025-05-11 12:08:52 +0100

    Did some more widget work.

--- app/src/main/java/com/nugget/hios/widget/WidgetProvider.java
+++ app/src/main/java/com/nugget/hios/widget/WidgetProvider.java
@@ -1,46 +1,50 @@
-/*import android.app.PendingIntent;
+package com.nugget.hios.widget;
+
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.nugget.hios.MainActivity;
 import com.nugget.hios.R;
 
 public class WidgetProvider extends AppWidgetProvider {
 
-    public static final String ACTION_HOME = "com.nugget.hios.ui.home.HomeFragment";
-    public static final String ACTION_RESTAURANT = "com.nugget.hios.ui.dashboard.DashboardFragment";
-    public static final String ACTION_HOTEL = "com.nugget.hios.ui.notifications.NotificationsFragment";
-    public static final String ACTION_ROOMKEY = "com.nugget.hios.ui.settings.SettingsFragment";
+    public static final String HOME_PAGE = "com.nugget.hios.ui.home.HomeFragment";
+    public static final String RESTAURANT_PAGE = "com.nugget.hios.ui.dashboard.DashboardFragment";
+    public static final String HOTEL_PAGE = "com.nugget.hios.ui.notifications.NotificationsFragment";
+    public static final String ROOMKEY_PAGE = "com.nugget.hios.ui.settings.SettingsFragment";
 
     @Override
-    public void onUpdate(Context context, AppWidgetProvider appWidgetProvider, int[] appWidgetIds) {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetProvider, appWidgetId);
+            updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 
-        //PendingIntents
-        Intent homeIntent = new Intent(ACTION_HOME);
-        PendingIntent homePendingIntent = PendingIntent.getBroadcast(context, 0, homeIntent, PendingIntent.FLAG_IMMUTABLE);
-        views.setOnClickPendingIntent(R.id.widget_button_home, homePendingIntent);
+        //create PendingIntents for each button
+        Intent homePageIntent = new Intent(HOME_PAGE);
+        PendingIntent homePagePendingIntent = PendingIntent.getBroadcast(context, 0, homePageIntent, PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.widget_home, homePagePendingIntent);
 
-        Intent restaurantIntent = new Intent(ACTION_RESTAURANT);
-        PendingIntent restaurantPendingIntent = PendingIntent.getBroadcast(context, 0, restaurantIntent, PendingIntent.FLAG_IMMUTABLE);
-        views.setOnClickPendingIntent(R.id.widget_button_restaurant, restaurantPendingIntent);
+        Intent restaurantPageIntent = new Intent(RESTAURANT_PAGE);
+        PendingIntent restaurantPagePendingIntent = PendingIntent.getBroadcast(context, 0, restaurantPageIntent, PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.widget_restaurant, restaurantPagePendingIntent);
 
-        Intent hotelIntent = new Intent(ACTION_HOTEL);
-        PendingIntent hotelPendingIntent = PendingIntent.getBroadcast(context, 0, hotelIntent, PendingIntent.FLAG_IMMUTABLE);
-        views.setOnClickPendingIntent(R.id.widget_button_hotel, hotelPendingIntent);
+        Intent hotelPageIntent = new Intent(HOTEL_PAGE);
+        PendingIntent hotelPagePendingIntent = PendingIntent.getBroadcast(context, 0, hotelPageIntent, PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.widget_hotel, hotelPagePendingIntent);
 
-        Intent roomkeyIntent = new Intent(ACTION_ROOMKEY);
-        PendingIntent roomkeyPendingIntent = PendingIntent.getBroadcast(context, 0, roomkeyIntent, PendingIntent.FLAG_IMMUTABLE);
-        views.setOnClickPendingIntent(R.id.widget_button_roomkey, roomkeyPendingIntent);
+        Intent roomkeyPageIntent = new Intent(ROOMKEY_PAGE);
+        PendingIntent roomkeyPagePendingIntent = PendingIntent.getBroadcast(context, 0, roomkeyPageIntent, PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.widget_roomkey, roomkeyPagePendingIntent);
 
+        //update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
-}*/
+}
